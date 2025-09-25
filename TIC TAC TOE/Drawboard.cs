0d49b80fe85f950fc55132f952040939958d9d77
gabriel hauso <gabrielhauso@gmail.com>
2025-09-25 17:40:23 +0200

    Spelplanen

---
+++ TIC TAC TOE/Drawboard.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIC_TAC_TOE
+{
+    internal class Drawboard
+    {
+        
+       
+
+
+
+        //public  static void SpelRitning()
+        //{
+
+
+        //    string[] board = new string[9] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+        //    bool player1 = true;
+
+
+        //    while (true)
+        //    {
+
+        //       if (player1)
+        //        {
+        //            Console.WriteLine("player 1 (X) tur");
+        //        }
+        //        else
+        //        {
+        //            Console.WriteLine("player 2 (O) tur");
+        //        }
+
+
+        //            string välj = Console.ReadLine();
+
+
+        //        if (board.Contains(välj) && välj != "X" && välj != "O")
+        //        {
+        //            int play = Convert.ToInt32(välj) - 1;
+
+
+        //           if (player1)
+        //            {
+        //                board[play] = "X";
+        //            }
+        //           else
+        //            {
+        //                board[play] = "O";
+        //            }
+
+        //        }
+        //        else
+        //        {
+        //            Console.WriteLine("Ogiltigt val, försök igen!");
+        //            continue;
+        //        }
+
+        //        if (player1)
+        //        {
+        //            player1 = false;
+        //        }
+        //        player1 = true;
+
+        //        Console.WriteLine($"|  {board[0]}  |  {board[1]}  |  {board[2]} |");
+        //        Console.WriteLine("|----------------|");
+        //        Console.WriteLine($"|  {board[3]}  |  {board[4]}  |  {board[5]} |");
+        //        Console.WriteLine("|----------------|");
+        //        Console.WriteLine($"|  {board[6]}  |  {board[7]}  |  {board[8]} |");
+        //    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //}
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- TIC TAC TOE/Program.cs
+++ TIC TAC TOE/Program.cs
@@ -11,47 +11,95 @@ namespace TIC_TAC_TOE
             Console.ReadLine();
             Console.WriteLine($"Spelare 1 = X och Spelare 2 = O");
 
-            
-            
-                             
-                SpelRitning();
+            Console.Clear();
 
-
-            
-            
            
+
+            SpelRitning();
+
+
+
         }
-        private static void SpelRitning()
+
+        public static void SpelRitning()
         {
-            
+
 
             string[] board = new string[9] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
             bool player1 = true;
+            
 
             while (true)
             {
-                if (player1)
-                    Console.WriteLine("Player 1 tur");
-                else
-                    Console.WriteLine("Player 2 tur");    
-                string välj = Console.ReadLine();
-                
-                if(board.Contains(välj) 
 
+                if (player1)
+                {
+                    Console.WriteLine("player 1 (X) tur");
+                }
+                else 
+                {
+                    Console.WriteLine("player 2 (O) tur");
+                }
+
+
+                string välj = Console.ReadLine();
+
+
+                if (board.Contains(välj) && välj != "X" && välj != "O")
+                {
+                    int play = Convert.ToInt32(välj) - 1;
+
+
+                    if (player1)
+                    {
+                        board[play] = "X";
+                    }
+                    else
+                    {
+                        board[play] = "O";
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Ogiltigt val, Försök igen!");
+                    continue;
+                }
+
+                if (player1)
+                {
+                    player1 = false;
+                }
+                else
+                {
+                    player1 = true;
+                }
+                Console.WriteLine($"|  {board[0]}  |  {board[1]}  |  {board[2]} |");
+                Console.WriteLine("|----------------|");
+                Console.WriteLine($"|  {board[3]}  |  {board[4]}  |  {board[5]} |");
+                Console.WriteLine("|----------------|");
+                Console.WriteLine($"|  {board[6]}  |  {board[7]}  |  {board[8]} |");
             }
 
-            Console.WriteLine($"|  {board[0]}  |  {board[1]}  |  {board[2]} |");
-            Console.WriteLine("|----------------|");
-            Console.WriteLine($"|  {board[3]}  |  {board[4]}  |  {board[5]} |");
-            Console.WriteLine("|----------------|");
-            Console.WriteLine($"|  {board[6]}  |  {board[7]}  |  {board[8]} |");
-            
-            
-            
-            
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
         }
+
+
+
     }
 }
